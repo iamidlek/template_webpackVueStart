ee593dea7e3fbfcdc4bf04f7794392efab9408ce
iamidlek <iamidlek@naver.com>
2021-10-03 12:36:03 +0900

    {

--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,11 @@ module.exports = {
         ]
       },
       {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
         test: /\.(png|jpe?g|gif|webp)$/,
         use: 'file-loader'
       },
